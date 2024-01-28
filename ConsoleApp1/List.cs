5670d7e4a2eced7c5395512a0a3dfe1068a7c308
Guilherme-Cavalanti <guilhermecavalanti@gmail.com>
2024-01-28 18:42:42 -0300

    first commit

---
+++ ConsoleApp1/List.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListPacks
+{
+    internal class TaskList
+    {
+        private List<Task> tasks;
+
+        public TaskList()
+        {
+            tasks = new List<Task>();
+        }
+        public int TaskCount
+        {
+            get { return tasks.Count; }
+        }
+        public void PrintTaks()
+        {
+            if (tasks.Count > 0)
+            {
+                Console.WriteLine();
+                foreach (var task in tasks)
+                {
+                    Console.WriteLine(task.ClassName);
+                }
+            }
+            else { Console.WriteLine("\nLista Vazia"); }
+        }
+        public void InsertTask(Task task)
+        {
+            tasks.Add(task);
+        }
+        public void RemoveTask()
+        {
+            if (tasks.Count > 0)
+            {
+                while (true)
+                {
+                    this.PrintTaks();
+                    Console.WriteLine($"Choose one to remove from 1 to {this.TaskCount} or type exit to quit.");
+                    string taskChoice = Console.ReadLine();
+                    if (taskChoice == "exit")
+                    {
+                        Console.WriteLine("Quitting...");
+                        break;
+                    }
+                    int numberTask;
+                    try
+                    {
+                        numberTask = int.Parse(taskChoice);
+                        if (numberTask < 1 || numberTask > this.TaskCount)
+                        {
+                            Console.WriteLine("Invalid number...");
+                            continue;
+                        }
+                        else
+                        {
+                            tasks.RemoveAt(numberTask);
+                            Console.WriteLine($"Task number {numberTask} removed succesfully!");
+                            break;
+                        }
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Wrong input must be a number");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nNo tasks to remove.");
+            }
+        }
+        public void DoTask()
+        {
+            if (tasks.Count > 0)
+            {
+                switch(tasks[0].ClassName)
+                {
+                    case "Eat":
+                        Console.WriteLine("\nYummy *eating sounds*\nThat was delicious!");
+                        break;
+                    case "Work":
+                        Console.WriteLine("\n*You're doing your work...*\nFinally finished this part!");
+                        break;
+                    case "Sleep":
+                        Console.WriteLine("\nZzZzZ *snores*\n*Wakes up* Im rested now =)");
+                        break;
+                }
+                tasks.RemoveAt(0);
+                Console.WriteLine("Task done succesfully");
+            }
+        }
+        public void StartTaskCycle()
+        {
+            bool end = false;
+            while (!end)
+            {
+                if (tasks.Count == 0) {
+                    Console.WriteLine("\nNo tasks to be done!");
+                    break;
+                }
+                Console.WriteLine($"\nThere are {tasks.Count} tasks(s) to be done.");
+                Console.WriteLine("Type 1 to do the next task");
+                Console.WriteLine("Type 2 to exit");
+                string input = Console.ReadLine();
+                switch(input)
+                {
+                    case "1":
+                        DoTask();
+                        break;
+                    case "2":
+                        Console.WriteLine("Quitting Task Cycle");
+                        end = true;
+                        break;
+                    default:
+                        Console.WriteLine("Must be 1 or 2");
+                        break;
+                }
+            }
+        }
+    }
+
+    internal class Task
+    {
+        private string className;
+        public string ClassName {   
+            get { return className; } 
+            set { className = value; }
+        }
+
+        public void CreateTask(int n)
+        {
+            switch (n)
+            {
+                case 0:
+                    ClassName = "Eat";
+                    break;
+                case 1:
+                    className = "Work";
+                    break;
+                case 2:
+                    className = "Sleep";
+                    break;
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using ListPacks;
+using Task = ListPacks.Task;
+using System.Threading.Tasks;
+
+class Program
+{   
+
+    static void Main()
+    {
+        TaskList lista = new TaskList();
+
+        bool exit = false;
+        while (!exit)
+        {
+            Console.WriteLine("\nTask Manager");
+            Console.WriteLine("1. Add Task");
+            Console.WriteLine("2. View Tasks");
+            Console.WriteLine("3. Remove Task");
+            Console.WriteLine("4. Do tasks");
+            Console.WriteLine("5. Clear Console");
+            Console.WriteLine("6. Exit");
+            Console.Write("Enter your choice: ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    if (lista.TaskCount == 6)
+                    {
+                        Console.WriteLine("\nMax length of 6 reached!");
+                        continue;
+                    }
+                    int n = AddTask();
+                    if(n == 3)
+                    {
+                        Console.WriteLine("Invalid Task, Restarting...");
+                        continue;
+                    }
+                    Task task = new Task();
+                    task.CreateTask(n);
+                    lista.InsertTask(task);
+                    Console.WriteLine("\nTask added");
+                    break;
+                case "2":
+                    lista.PrintTaks();
+                    break;
+                case "3":
+                    lista.RemoveTask();
+                    break;
+                case "4":
+                    lista.StartTaskCycle();
+                    break;
+                case "5":
+                    Console.Clear();
+                    break;
+                case "6":
+                    exit = true;
+                    Console.WriteLine("\nQuitting");
+                    break;
+                default:
+                    Console.WriteLine("\nInvalid choice. Please enter a number from 1 to 5.");
+                    break;
+            }
+        }
+    }
+    static int AddTask()
+    {
+        Console.WriteLine("\nWhich Task:");
+        Console.WriteLine("0. Eat");
+        Console.WriteLine("1. Work");
+        Console.WriteLine("2. Sleep");
+        Console.WriteLine("Enter task:");
+        string task = Console.ReadLine();
+        switch(task)
+        {
+            case "0":
+                return 0;
+            case "1":
+                return 1;
+            case "2":
+                return 2;
+            default:
+                Console.WriteLine("Invalid Input!");
+                return 3;
+        }
+    }
+}
